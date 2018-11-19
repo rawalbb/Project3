6f674232962673b5c9e53d43e0cad2299cdfc352
Vishakha Sehgal <sehgalv@vcu.edu>
2018-11-18 20:08:07 -0500

    updates reading files

--- src/Proj3.java
+++ src/Proj3.java
@@ -8,39 +8,33 @@ public class Proj3
 {
 
     public static void main(String[] args) throws IOException {
-        List<Stats> allData = readData();
-    }
-
-    private static List<Stats> readData() throws IOException {
         ArrayList<Stats> train1 = new ArrayList<>();
         ArrayList<Stats> train2 = new ArrayList<>();
         ArrayList<Stats> train3 = new ArrayList<>();
         ArrayList<Stats> test = new ArrayList<>();
-        File file1 = new File("/Users/Bansri/Desktop/CSAI/Project3/train_data_1.txt");
+
+        train1 = readData("train_data_1.txt");
+        train2 = readData("train_data_2.txt");
+        train3 = readData("train_data_3.txt");
+        test = readData("test_data_4.txt");
+    }
+
+    private static ArrayList<Stats> readData(String fileName) throws IOException {
+        ArrayList<Stats> allDataPoints = new ArrayList<>();
+        File file1 = new File(fileName);
         Scanner scanFile = new Scanner(file1);
 
         while (scanFile.hasNextLine()) {
             String dataPoint = scanFile.nextLine();
             String[] data = dataPoint.split(", ");
-            train1.add(new Stats(Double.parseDouble(data[0]), Double.parseDouble(data[1])));
+            allDataPoints.add(new Stats(Double.parseDouble(data[0]), Double.parseDouble(data[1])));
         }
+        scanFile.close();
 
-        File file2 = new File("/Users/Bansri/Desktop/CSAI/Project3/train_data_2.txt");
-        Scanner scanFile2 = new Scanner(file2);
-
-        File file3 = new File("/Users/Bansri/Desktop/CSAI/Project3/train_data_3.txt");
-        Scanner scanFile3 = new Scanner(file3);
-
-        File file4 = new File("/Users/Bansri/Desktop/CSAI/Project3/test_data_4.txt");
-        Scanner scanFile4 = new Scanner(file4);
-
-        return train3;
-
+        return allDataPoints;
     }
 
-
 }
-
 
 class Stats{
 
