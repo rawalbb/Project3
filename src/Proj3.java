fe01e4c0c3443834ea884e039ff1595cf65eaf36
Vishakha Sehgal <sehgalv@vcu.edu>
2018-11-19 22:54:00 -0500

    updates weights and iteration counts

--- src/Proj3.java
+++ src/Proj3.java
@@ -1,11 +1,11 @@
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
-public class Proj3
-{
+public class Proj3 {
 
     public static void main(String[] args) throws IOException {
         ArrayList<Stats> train1 = new ArrayList<>();
@@ -13,14 +13,14 @@ public class Proj3
         ArrayList<Stats> train3 = new ArrayList<>();
         ArrayList<Stats> test = new ArrayList<>();
 
-        train1 = readData("train_data_1.txt");
-        train2 = readData("train_data_2.txt");
-        train3 = readData("train_data_3.txt");
-        test = readData("test_data_4.txt");
-        double training_const = 0.005;
+        train1 = normalizeData(readData("train_data_1.txt"));
+        train2 = normalizeData(readData("train_data_2.txt"));
+        train3 = normalizeData(readData("train_data_3.txt"));
+        test = normalizeData(readData("test_data_4.txt"));
+        double training_const = 0.000001;
 
         System.out.println("\n\nlinear architecture------------------");
-        double[] linearWeights = {1,2,3};
+        double[] linearWeights = {1,0,1};
         System.out.println("\n\nTraining set 1: ");
         linearWeights = linearLearning(train1, linearWeights, training_const);
         System.out.println("\n\nTraining set 2: ");
@@ -31,7 +31,7 @@ public class Proj3
         linearTesting(test, linearWeights);
 
         System.out.println("\n\nquadratic architecture------------------");
-        double[] quadraticWeights = {1,2,3};
+        double[] quadraticWeights = {1,0,1};
         System.out.println("\n\nTraining set 1: ");
         quadraticWeights = quadraticLearning(train1, quadraticWeights, training_const);
         System.out.println("\n\nTraining set 2: ");
@@ -41,8 +41,8 @@ public class Proj3
         System.out.println("\n\nTest: ");
         quadraticTesting(test, quadraticWeights);
 
-        System.out.println("cubic architecture------------------");
-        double[] cubicWeights = {1,2,3,4};
+        System.out.println("\n\ncubic architecture------------------");
+        double[] cubicWeights = {1,-1,1,-1};
         System.out.println("\n\nTraining set 1: ");
         cubicWeights = quadraticLearning(train1, cubicWeights, training_const);
         System.out.println("\n\nTraining set 2: ");
@@ -51,7 +51,6 @@ public class Proj3
         cubicWeights = cubicLearning(train3, cubicWeights, training_const);
         System.out.println("\n\nTest: ");
         cubicTesting(test, cubicWeights);
-
     }
 
     private static ArrayList<Stats> readData(String fileName) throws IOException {
@@ -69,21 +68,66 @@ public class Proj3
         return allDataPoints;
     }
 
-    //while X iterations
+    private static ArrayList<Stats> normalizeData(ArrayList<Stats> toNormalize) {
+        ArrayList<Stats> normalized = new ArrayList<Stats>();
+        double minHours = 0;
+        double maxHours = 0;
+        double minRate = 0;
+        double maxRate = 0;
+
+        for(Stats row: toNormalize) {
+            if(maxHours == 0) {
+                minHours = row.getHour();
+                maxHours = row.getHour();
+            }
+
+            if(maxRate == 0) {
+                minRate = row.getRate();
+                maxRate = row.getRate();
+            }
+
+            if(row.getHour() > maxHours) {
+                maxHours = row.getHour();
+            }
+
+            if(row.getHour() < minHours) {
+                minHours = row.getHour();
+            }
+
+            if(row.getRate() > maxRate) {
+                maxRate = row.getRate();
+            }
+
+            if(row.getRate() < minRate) {
+                minRate = row.getRate();
+            }
+        }
+
+        for(Stats row: toNormalize) {
+            double normalizedHour = (row.getHour() - minHours) / (maxHours-minHours);
+            double normalizedRate = (row.getRate() - minRate) / (maxRate-minRate);
+            normalized.add(new Stats(normalizedHour, normalizedRate));
+        }
+
+        return normalized;
+    }
+
     public static double[] linearLearning(ArrayList<Stats> train, double[] weight, double learning_const) {
-        //call total error method, if epsilon > total error, break
-        //new weights = calc new weights, weights = new weights
         double out;
         double weightMultiplier = 1;
         double totalError = 0;
-        for (int i = 0; i < train.size(); i++) {
-            out = train.get(i).getHour() * weight[0] + weight[1];
-            weightMultiplier = learning_const * (train.get(i).getRate() - out);
-            weight[0] +=  weightMultiplier * train.get(i).getHour();
-            weight[1] += weightMultiplier;
-            totalError += Math.pow(train.get(i).getRate() - out , 2);
+        int count  = 0;
+        while(count < 201) {
+            for (int i = 0; i < train.size(); i++) {
+                out = train.get(i).getHour() * weight[0] + weight[1];
+                weightMultiplier = learning_const * (train.get(i).getRate() - out);
+                weight[0] += weightMultiplier * train.get(i).getHour();
+                weight[1] += weightMultiplier;
+                totalError += Math.pow(train.get(i).getRate() - out, 2);
+            }
+            count++;
         }
-        System.out.println("Total Error: " + totalError + " : RootMeanError = " + Math.sqrt(totalError) / train.size());
+        System.out.println("Mean Square Error = " + Math.sqrt(totalError) / train.size());
         return weight;
     }
 
@@ -100,84 +144,86 @@ public class Proj3
 
             testTotalError += Math.pow((test.get(i).getRate() - testOut), 2);
         }
-        System.out.println("Total Error: " + testTotalError + " : Mean Square Error = " + Math.sqrt(testTotalError)/ test.size());
+        System.out.println("Mean Square Error = " + Math.sqrt(testTotalError)/ test.size());
     }
 
     public static double[] quadraticLearning(ArrayList<Stats> train, double[] weight, double learning_const) {
         double out;
         double weightMultiplier = 1;
         double totalError = 0;
+        int count = 0;
 
         for(double w : weight) {
             System.out.println(w);
         }
 
-        for (int i = 0; i < train.size(); i++) {
-            out = (Math.pow(train.get(i).getHour(),2) * weight[0]) + (train.get(i).getHour() * weight[1]) + weight[2];
-            weightMultiplier = learning_const * (train.get(i).getRate() - out);
-            weight[0] += weightMultiplier * train.get(i).getHour();
-            weight[1] += weightMultiplier * train.get(i).getHour();
-            weight[2] += weightMultiplier;
-            totalError += Math.pow(train.get(i).getRate() - out , 2);
+        while(count <= 1000000) {
+            for (int i = 0; i < train.size(); i++) {
+                out = (Math.pow(train.get(i).getHour(), 2) * weight[0]) + (train.get(i).getHour() * weight[1]) + weight[2];
+                weightMultiplier = learning_const * (train.get(i).getRate() - out);
+                weight[0] += weightMultiplier * Math.pow(train.get(i).getHour(), 2);
+                weight[1] += weightMultiplier * train.get(i).getHour();
+                weight[2] += weightMultiplier;
+                totalError += Math.pow(train.get(i).getRate() - out, 2);
+            }
+            count++;
         }
-        System.out.println("Total Error: " + totalError + " : RootMeanError = " + Math.sqrt(totalError) / train.size());
+        System.out.println("Mean Square Error = " + Math.sqrt(totalError) / train.size());
         return weight;
     }
 
     public static void quadraticTesting(ArrayList<Stats> test, double[] weight) {
         double testOut;
         ArrayList<Stats> results = new ArrayList<>();
-        double incorrect = 0;
         double testTotalError = 0;
         for(double w : weight) {
             System.out.println(w);
         }
-        for (int i = 0; i < test.size(); i++) {
-            testOut = (Math.pow(test.get(i).getHour(),2) * weight[0]) + (test.get(i).getHour() * weight[1]) + weight[2];
-            results.add(new Stats(test.get(i).getHour(), testOut));
+            for (int i = 0; i < test.size(); i++) {
+                testOut = (Math.pow(test.get(i).getHour(), 2) * weight[0]) + (test.get(i).getHour() * weight[1]) + weight[2];
+                results.add(new Stats(test.get(i).getHour(), testOut));
 
-            System.out.println("Expected: " + test.get(i).getRate()+ " Predicted: " + testOut);
+                System.out.println("Expected: " + test.get(i).getRate() + " Predicted: " + testOut);
 
-            testTotalError += Math.pow((test.get(i).getRate() - testOut), 2);
-        }
-        System.out.println("Total Error: " + testTotalError + " : Mean Square Error = " + Math.sqrt(testTotalError)/ test.size());
+                testTotalError += Math.pow((test.get(i).getRate() - testOut), 2);
+            }
+        System.out.println("Mean Square Error = " + Math.sqrt(testTotalError)/ test.size());
     }
 
     public static double[] cubicLearning(ArrayList<Stats> train, double[] weight, double learning_const) {
         double out;
         double weightMultiplier = 1;
         double totalError = 0;
-        for (int i = 0; i < train.size(); i++) {
-            out = (Math.pow(train.get(i).getHour(),3) * weight[0]) + (Math.pow(train.get(i).getHour(),2) * weight[1]) + (train.get(i).getHour() * weight[2]) + weight[3];
-            weightMultiplier = learning_const * (train.get(i).getRate() - out);
-            weight[0] += weightMultiplier * Math.pow(train.get(i).getHour(), 2);
-            weight[1] += weightMultiplier * train.get(i).getHour();
-            weight[2] += weightMultiplier;
-            totalError += Math.pow(train.get(i).getRate() - out , 2);
+        int count = 0;
+        while(count < 1000001) {
+            for (int i = 0; i < train.size(); i++) {
+                out = (Math.pow(train.get(i).getHour(), 3) * weight[0]) + (Math.pow(train.get(i).getHour(), 2) * weight[1]) + (train.get(i).getHour() * weight[2]) + weight[3];
+                weightMultiplier = learning_const * (train.get(i).getRate() - out);
+                weight[0] += weightMultiplier * Math.pow(train.get(i).getHour(), 3);
+                weight[1] += weightMultiplier * Math.pow(train.get(i).getHour(), 2);
+                weight[2] += weightMultiplier * train.get(i).getHour();
+                weight[3] += weightMultiplier;
+                totalError += Math.pow(train.get(i).getRate() - out, 2);
+            }
+            count++;
         }
-        System.out.println("Total Error: " + totalError + " : RootMeanError = " + Math.sqrt(totalError) / train.size());
+        System.out.println("Mean Square Error = " + Math.sqrt(totalError) / train.size());
         return weight;
     }
 
     public static void cubicTesting(ArrayList<Stats> test, double[] weight) {
         double testOut;
         ArrayList<Stats> results = new ArrayList<>();
-        double incorrect = 0;
         double testTotalError = 0;
         for (int i = 0; i < test.size(); i++) {
             testOut = (Math.pow(test.get(i).getHour(),3) * weight[0]) + (Math.pow(test.get(i).getHour(),2) * weight[1]) + (test.get(i).getHour() * weight[2]) + weight[3];
             results.add(new Stats(test.get(i).getHour(), testOut));
 
-            System.out.println("Expected: " + test.get(i).getRate()+ " Predicted: " + testOut);
-            if (test.get(i).getRate() != testOut){
-                incorrect++;
-            }
             testTotalError += Math.pow((test.get(i).getRate() - testOut), 2);
+            System.out.println("Expected: " + test.get(i).getRate()+ " Predicted: " + testOut);
         }
-        System.out.println("Incorrect: " + incorrect + "  Total Error: " + testTotalError + "  Mean Square Error = " + Math.sqrt(testTotalError)/ test.size());
+        System.out.println("Mean Square Error = " + Math.sqrt(testTotalError)/ test.size());
     }
-
-
 }
 
 class Stats {
